8844c52909829c157f117ba36b21b243dea09e3f
Thomas Gelf <thomas@gelf.net>
2024-09-20 11:59:27 +0200

    Measurement: drop unused functionality

--- src/Measurement.php
+++ src/Measurement.php
@@ -4,8 +4,7 @@ namespace IMEdge\Metrics;
 
 use IMEdge\Json\JsonSerialization;
 use IMEdge\Json\JsonString;
-use InvalidArgumentException;
-use RuntimeException;
+use JsonException;
 
 class Measurement implements JsonSerialization
 {
@@ -28,71 +27,12 @@ class Measurement implements JsonSerialization
         }
     }
 
-    public function setGaugeValue(string $name, $value, ?string $unit = null): void
-    {
-        $this->getMetric($name, MetricDatatype::GAUGE, $unit)->value = $value;
-    }
-
-    public function incrementCounter(string $name, float|int $increment = 1): void
-    {
-        $metric = $this->getMetric($name, MetricDatatype::COUNTER);
-        $metric->value = $metric->value + $increment;
-    }
-
     /**
      * @return Metric[]
      */
     public function getMetrics(): array
     {
         return $this->metrics;
-    }
-
-    public function getMetric(string $name, ?MetricDatatype $type = null, ?string $unit = null): Metric
-    {
-        if (isset($this->metrics[$name])) {
-            // TODO: adapt unit?!
-            if ($this->metrics[$name]->type !== $type) {
-                throw new RuntimeException(sprintf(
-                    '%s requested for "%s", got %s',
-                    $type->value,
-                    $name,
-                    $this->metrics[$name]->type->value
-                ));
-            }
-            return $this->metrics[$name];
-        }
-
-        return $this->metrics[$name] = new Metric($name, $type, $unit);
-    }
-
-    public function resetCounters(float|int|null $resetTo = null): void
-    {
-        foreach ($this->metrics as $metric) {
-            if ($metric->type === MetricDatatype::COUNTER) {
-                $metric->value = $resetTo;
-            }
-        }
-    }
-
-    public function reset(int|float|null $resetTo = null): void
-    {
-        foreach ($this->metrics as $metric) {
-            $metric->value = $resetTo;
-        }
-    }
-
-    public function has(string $label): bool
-    {
-        return isset($this->metrics[$label]);
-    }
-
-    public function requireMetric(string $label): Metric
-    {
-        if (isset($this->metrics[$label])) {
-            return $this->metrics[$label];
-        }
-
-        throw new InvalidArgumentException("There is no such DataPoint: '$label'");
     }
 
     public function addMetric(Metric $metric): void
@@ -110,6 +50,10 @@ class Measurement implements JsonSerialization
         return $this->timestamp;
     }
 
+    /**
+     * @return array{string, ?int, Metric[]}
+     * @throws JsonException
+     */
     public function jsonSerialize(): array
     {
         return [
@@ -119,9 +63,9 @@ class Measurement implements JsonSerialization
         ];
     }
 
-    public static function fromSerialization($any): self
+    public static function fromSerialization($any): Measurement
     {
-        return new static(
+        return new Measurement(
             Ci::fromSerialization(JsonString::decode($any[0])),
             $any[1] ?? null,
             array_map(Metric::fromSerialization(...), (array) $any[2])
